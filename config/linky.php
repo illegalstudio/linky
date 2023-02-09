d90201369f18cc30305870eecb283a2dc7f4e56c
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-02-09 21:28:27 +0100

    Added configuration and first migration

---
+++ config/linky.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'db' => [
+        'prefix' => env('LINKY_DB_PREFIX', 'linky_'),
+    ]
+];
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,11 +10,23 @@ class ServiceProvider extends IlluminateServiceProvider
      * @return void
      */
     public function register(): void
-    { }
+    {
+        /**
+         * Bennu config file
+         */
+        $this->mergeConfigFrom(__DIR__ . "/../config/linky.php", "linky");
+    }
 
     /**
      * @return void
      */
     public function boot(): void
-    { }
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'linky');
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'linky');
+
+        $this->loadMigrationsFrom([
+            __DIR__ . '/../database/migrations/'
+        ]);
+    }
 }
