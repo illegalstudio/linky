871933a1c56fe7841d0ec54011c9716cf67a94be
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-02-10 23:43:21 +0100

    Registered all commands

--- src/RouteServiceProvider.php
+++ src/RouteServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Illegal\Linky;
 
+use Illegal\Linky\Commands\CreateCollectionCommand;
+use Illegal\Linky\Commands\CreatePageCommand;
 use Illegal\Linky\Commands\CreateRedirectCommand;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as IlluminateRouteServiceProvider;
 
@@ -11,6 +13,8 @@ class RouteServiceProvider extends IlluminateRouteServiceProvider
     public function boot(): void
     {
         $this->commands([
+            CreateCollectionCommand::class,
+            CreatePageCommand::class,
             CreateRedirectCommand::class
         ]);
     }
