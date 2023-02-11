72a5491a4004b74b9d572f0edcf5063c1b42c701
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-02-11 23:20:32 +0100

    Adapted tailwind layout

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -7,7 +7,36 @@
 </head>
 <body>
 
-@yield('content')
+<header class="bg-indigo-600 mb-10">
+    <nav class="mx-auto max-w-7xl px-6 lg:px-8" aria-label="Top">
+        <div class="flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none">
+            <div class="flex items-center">
+                <a href="#">
+                    <span class="sr-only">Your Company</span>
+                    <img class="h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=white" alt="">
+                </a>
+                <div class="ml-10 hidden space-x-8 lg:block">
+                    <a href="#" class="text-base font-medium text-white hover:text-indigo-50">Links</a>
+                    <a href="#" class="text-base font-medium text-white hover:text-indigo-50">Collections</a>
+                    <a href="#" class="text-base font-medium text-white hover:text-indigo-50">Pages</a>
+                </div>
+            </div>
+            <div class="ml-10 space-x-4">
+                <a href="#" class="inline-block rounded-md border border-transparent bg-indigo-500 py-2 px-4 text-base font-medium text-white hover:bg-opacity-75">Logout</a>
+                <!--<a href="#" class="inline-block rounded-md border border-transparent bg-white py-2 px-4 text-base font-medium text-indigo-600 hover:bg-indigo-50">Logout</a>-->
+            </div>
+        </div>
+        <div class="flex flex-wrap justify-center gap-x-6 py-4 lg:hidden">
+            <a href="#" class="text-base font-medium text-white hover:text-indigo-50">Links</a>
+            <a href="#" class="text-base font-medium text-white hover:text-indigo-50">Collections</a>
+            <a href="#" class="text-base font-medium text-white hover:text-indigo-50">Pages</a>
+        </div>
+    </nav>
+</header>
+
+<div class="mx-auto max-w-7xl">
+    @yield('content')
+</div>
 
 @livewireScripts
 </body>
--- resources/views/admin/link/index.blade.php
+++ resources/views/admin/link/index.blade.php
@@ -1,39 +1,58 @@
 @extends('linky::admin.layout')
 
 @section('content')
-    <div class="container mx-auto">
-        <div class="flex justify-between">
-            <h1 class="text-2xl font-bold">Links</h1>
-            <a href="{{ route('linky.admin.link.create') }}" class="btn btn-primary">Create</a>
+
+    <div class="px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:items-center">
+            <div class="sm:flex-auto">
+                <h1 class="text-xl font-semibold text-gray-900">Links</h1>
+                <p class="mt-2 text-sm text-gray-700">A list of all the links in your account</p>
+            </div>
+            <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                <a href="{{ route('linky.admin.link.create') }}" type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add link</a>
+            </div>
         </div>
-        <div class="mt-4">
-            <table class="table-auto w-full">
-                <thead>
-                    <tr>
-                        <th class="px-4 py-2">Name</th>
-                        <th class="px-4 py-2">Slug</th>
-                        <th class="px-4 py-2">Url</th>
-                        <th class="px-4 py-2">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($links as $link)
-                        <tr>
-                            <td class="border px-4 py-2">{{ $link->content->name }}</td>
-                            <td class="border px-4 py-2">{{ $link->content->slug }}</td>
-                            <td class="border px-4 py-2">{{ $link->url }}</td>
-                            <td class="border px-4 py-2">
-                                <a href="{{ route('linky.admin.link.edit', $link) }}" class="btn btn-primary">Edit</a>
-                                <form action="{{ route('linky.admin.link.destroy', $link) }}" method="POST" class="inline">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="mt-8 flex flex-col">
+            <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                    <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-300">
+                            <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Slug</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">URL</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
+                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                    <span class="sr-only">Actions</span>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200 bg-white">
+                            @foreach ($links as $link)
+                            <tr>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                                    <div class="font-medium text-gray-900">{{ $link->content->name }}</div>
+                                </td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    <div class="text-gray-900">{{ $link->content->slug }}</div>
+                                </td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    <div class="text-gray-900">{{ $link->url }}</div>
+                                </td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    <span class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">{{ $link->content->status }}</span>
+                                </td>
+                                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
+                                </td>
+                            </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
