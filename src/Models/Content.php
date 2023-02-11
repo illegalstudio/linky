fb92cb1635b0c53703fc096b7409a9824d2be2c3
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-02-11 17:45:45 +0100

    Added casts

--- src/Models/Content.php
+++ src/Models/Content.php
@@ -3,6 +3,8 @@
 namespace Illegal\Linky\Models;
 
 use Illegal\Linky\Abstracts\AbstractModel;
+use Illegal\Linky\Enums\ContentStatus;
+use Illegal\Linky\Enums\ContentType;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Content extends AbstractModel
@@ -11,6 +13,11 @@ class Content extends AbstractModel
      * @var string $tableName The table associated with the model.
      */
     protected $tableName = 'contents';
+
+    public $casts = [
+        'status' => ContentStatus::class,
+        'type'   => ContentType::class
+    ];
 
     /**
      * @return MorphTo
