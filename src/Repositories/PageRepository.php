861832364c3f6955151da35b15db60c9db0f4089
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-02-12 16:26:49 +0100

    Start page repository

--- src/Repositories/PageRepository.php
+++ src/Repositories/PageRepository.php
@@ -3,6 +3,7 @@
 namespace Illegal\Linky\Repositories;
 
 use Illegal\Linky\Abstracts\AbstractRepository;
+use Illegal\Linky\Enums\ContentStatus;
 use Illegal\Linky\Enums\ContentType;
 use Illegal\Linky\Models\Content;
 use Illegal\Linky\Models\Contentable\Page;
@@ -13,13 +14,27 @@ final class PageRepository extends AbstractRepository
      * Create a new page.
      *
      * @param array $data
+     * @param ContentStatus $status
+     * @param string|null $slug
+     * @param string|null $name
+     * @param string|null $description
      * @return Content
      */
-    public static function create(array $data = []): Content
+    public static function create(
+        array         $data = [],
+        ContentStatus $status = ContentStatus::Draft,
+        string        $slug = null,
+        string        $name = null,
+        string        $description = null
+    ): Content
     {
         return parent::createContent(
             Page::forceCreate($data),
-            ContentType::Page
+            ContentType::Page,
+            $status,
+            $slug,
+            $name,
+            $description
         );
     }
 }
