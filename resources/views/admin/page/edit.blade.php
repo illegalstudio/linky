882c3e102b15b83ef20345f90fabc8841caf1086
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-02-12 16:26:14 +0100

    Fixed variable name

--- resources/views/admin/page/edit.blade.php
+++ resources/views/admin/page/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     @include('linky::admin.page._parts.form', [
-        'action' => route('linky.admin.page.update', $link),
+        'action' => route('linky.admin.page.update', $page),
         'method' => 'PUT',
         'title' => 'Update page',
         'subtitle' => 'Update existing page',
