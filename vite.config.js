8c7c0a60fadb6968e1ad1a1c1c6b17b42964d9bb
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-02-15 00:26:25 +0100

    Changed rollupOptions to build assets without hash

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: "assets/[name].[ext]",
+                entryFileNames: "assets/[name].js",
+                chunkFileNames: "assets/[name].js",
+            },
+        },
+    },
     plugins: [
         laravel({
             input: [
