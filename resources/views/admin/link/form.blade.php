e0e73a275439e8c012861697453242f774895994
Andrea Pavone <info@andreapavone.com>
2023-02-21 23:27:38 +0100

    Remove disaply block / Remove right only rounded border on text input
    Signed-off-by: Andrea Pavone <info@andreapavone.com>

--- resources/views/admin/link/form.blade.php
+++ resources/views/admin/link/form.blade.php
@@ -1,8 +1,8 @@
 <div>
-    <label for="url" class="block text-sm font-medium text-gray-700">URL</label>
+    <label for="url" class="text-sm font-medium text-gray-700">URL</label>
     <div class="mt-1">
         <input type="text" name="url" id="url"
-               class="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+               class="w-full flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                value="{{ old('url') ?? $link->url ?? "" }}"
                placeholder="https://">
     </div>
--- resources/views/admin/page/form.blade.php
+++ resources/views/admin/page/form.blade.php
@@ -1,8 +1,8 @@
 <div>
-    <label for="body" class="block text-sm font-medium text-gray-700">Body</label>
+    <label for="body" class="text-sm font-medium text-gray-700">Body</label>
     <div class="mt-1">
                             <textarea id="body" name="body" rows="3"
-                                      class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                      class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                       placeholder="...">{{ old('body') ?? $page->body ?? "" }}</textarea>
     </div>
     <p class="mt-2 text-sm text-gray-500">The body or your page</p>
