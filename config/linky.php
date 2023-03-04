630b641940297c4925a55e0f62988fb14aeeee0d
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-04 20:16:25 +0100

    Adds flags to configuration to enable / disable auth related functionalities Protects routes that haven't to be registered if certain funcionaly has been disabled

--- config/linky.php
+++ config/linky.php
@@ -7,6 +7,13 @@ return [
         'require_valid_user' => env('LINKY_AUTH_REQUIRE_VALID_USER', true),
         'multi_tenant'       => env('LINKY_AUTH_MULTI_TENANT', false),
         'login_route_name'   => env('LINKY_AUTH_LOGIN_ROUTE_NAME', 'login'),
+        'functionalities'    => [
+            'register'           => env('LINKY_AUTH_FUNCTIONALITIES_REGISTER', true),
+            'forgot_password'    => env('LINKY_AUTH_FUNCTIONALITIES_FORGOT_PASSWORD', true),
+            'reset_password'     => env('LINKY_AUTH_FUNCTIONALITIES_RESET_PASSWORD', true),
+            'email_verification' => env('LINKY_AUTH_FUNCTIONALITIES_EMAIL_VERIFICATION', true),
+            'user_profile'       => env('LINKY_AUTH_FUNCTIONALITIES_USER_PROFILE', true),
+        ]
     ],
     'db'        => [
         'prefix' => env('LINKY_DB_PREFIX', 'linky_'),
--- routes/auth.php
+++ routes/auth.php
@@ -13,54 +13,52 @@ use Illegal\Linky\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('linky-guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
-
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-        ->name('password.request');
+    if (config('linky.auth.functionalities.register')) {
+        Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+        Route::post('register', [RegisteredUserController::class, 'store']);
+    }
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->name('password.email');
+    if (config('linky.auth.functionalities.forgot_password')) {
+        Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+        Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+    }
 
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name('password.reset');
-
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
+    if (config('linky.auth.functionalities.reset_password')) {
+        Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+        Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+    }
 });
 
 Route::middleware('linky-auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
-        ->name('verification.notice');
 
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-        ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
+    if (config('linky.auth.functionalities.email_verification')) {
 
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware('throttle:6,1')
-        ->name('verification.send');
+        Route::get('verify-email', EmailVerificationPromptController::class)->name('verification.notice');
+        Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+            ->middleware(['signed', 'throttle:6,1'])
+            ->name('verification.verify');
 
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
+        Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+            ->middleware('throttle:6,1')
+            ->name('verification.send');
 
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+        Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])->name('password.confirm');
+        Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+        Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+    }
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->name('logout');
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
 
 Route::middleware('linky-auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    if (config('linky.auth.functionalities.user_profile')) {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    }
 });
