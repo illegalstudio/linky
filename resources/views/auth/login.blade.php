b8c342828130ea45355c2b9158ea1a7ab7d71744
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-05 16:15:38 +0100

    Added Register button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,7 +30,7 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
+            @if (config('linky.auth.functionalities.forgot_password'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
@@ -40,5 +40,14 @@
                 {{ __('Log in') }}
             </x-linky::button-primary>
         </div>
+
+        <div class="mt-10 flex flex-col items-center sm:flex-col sm:items-start sm:mt-2">
+            @if (config('linky.auth.functionalities.register'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Dont\'t have an account? Register here') }}
+                </a>
+            @endif
+        </div>
+
     </form>
 </x-linky::layout>
