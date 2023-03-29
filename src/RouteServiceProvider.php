74f23ef0f4d96496829e5f3e4a55899cd1d16c50
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-29 20:25:32 +0200

    Removed routes exposed by InsideAuth

--- src/RouteServiceProvider.php
+++ src/RouteServiceProvider.php
@@ -2,15 +2,9 @@
 
 namespace Illegal\Linky;
 
-use Illegal\InsideAuth\Http\Middleware\Authenticate;
-use Illegal\InsideAuth\Http\Middleware\EncryptCookies;
-use Illegal\InsideAuth\Http\Middleware\RedirectIfAuthenticated;
-use Illegal\InsideAuth\Http\Middleware\VerifyCsrfToken;
-use Illegal\InsideAuth\InsideAuth;
 use Illegal\Linky\Commands\CreateCollectionCommand;
 use Illegal\Linky\Commands\CreateLinkCommand;
 use Illegal\Linky\Commands\CreatePageCommand;
-use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as IlluminateRouteServiceProvider;
 use Illuminate\Support\Facades\Route;
 
@@ -33,11 +27,6 @@ class RouteServiceProvider extends IlluminateRouteServiceProvider
             CreatePageCommand::class,
             CreateLinkCommand::class
         ]);
-
-        if (config('linky.auth.use_linky_auth') && config('linky.auth.require_valid_user')) {
-            Route::middleware(LinkyAuth::webMiddleware())
-                ->group(__DIR__ . '/../routes/auth.php');
-        }
 
         Route::middleware(LinkyAuth::webMiddleware())
             ->group(__DIR__ . '/../routes/web.php');
