a22df51680ff998ee06bb92c3434e33aa5213035
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-04-23 19:31:19 +0200

    Updated size of links in collection page

--- resources/views/public/collection/default.blade.php
+++ resources/views/public/collection/default.blade.php
@@ -5,7 +5,7 @@
 @section('body')
     <div class="flex flex-col w-full h-full justify-center gap-4 bg-gray-50">
         @foreach($collection->contents as $content )
-            <a class="w-[360px] max-w-md mx-auto bg-indigo-500 p-4 text-white rounded-md border-indigo-900 border-2 drop-shadow-md hover:bg-indigo-600"
+            <a class="w-[360px] max-w-[100%] mx-auto bg-indigo-500 p-4 text-white rounded-md border-indigo-900 border-2 drop-shadow-md hover:bg-indigo-600"
                 href="/{{ $content->slug }}" target="_blank">
                 {{ $content->name ?: $content->slug }}
             </a>
