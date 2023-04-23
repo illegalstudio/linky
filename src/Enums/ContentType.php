67b24365be94f306129a24851145a4d235b9a0c1
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-04-23 20:03:38 +0200

    Render a content based on contentType

--- src/Enums/ContentType.php
+++ src/Enums/ContentType.php
@@ -2,9 +2,33 @@
 
 namespace Illegal\Linky\Enums;
 
+use Illegal\Linky\Http\Renderers\AbortRenderer;
+use Illegal\Linky\Http\Renderers\CollectionRenderer;
+use Illegal\Linky\Http\Renderers\LinkRenderer;
+use Illegal\Linky\Http\Renderers\PageRenderer;
+use Illegal\Linky\Models\Content;
+use Illuminate\Http\Request;
+
 enum ContentType: string
 {
     case Link = "link";
     case Collection = "collection";
     case Page = "page";
+
+    /**
+     * Render the content.
+     * This function will return the rendered content, using the
+     * appropriate renderer - based on the content type - or abort with a 404 if the content type is not supported.
+     */
+    public function render(Request $request, Content $content)
+    {
+        $renderer = match ($this) {
+            self::Link       => LinkRenderer::class,
+            self::Collection => CollectionRenderer::class,
+            self::Page       => PageRenderer::class,
+            default          => AbortRenderer::class,
+        };
+
+        return (new $renderer)->handle($request, $content);
+    }
 }
